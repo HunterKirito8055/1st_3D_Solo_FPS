6f1d7d8fd3830968f6c0b1d546b21a5d4b9c596a
HunterKirito8055 <sirikondavishal8055@gmail.com>
2020-03-04 23:06:34 +0530

    h,

--- FPS Survival/Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ FPS Survival/Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,8 +6,22 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController character_controller;
     private Vector3 move_direction;
+   
+    public float move =5f ; //default running speed
 
-    public float speed = 5f;
+    [SerializeField]
+    private Transform Look_Root;    //Look root has main camera
+    public float run = 5f;
+   
+    public float sprint_speed = 7f;
+    public float crouch_speed = 2.5f;
+
+    private float stand_height = 1.8f;
+    private float crouch_height = 1.2f;
+
+    [SerializeField]
+    private bool is_Crouching;
+
     private float gravity =20f;
 
     public float jump_force = 10f;
@@ -15,12 +29,15 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         character_controller = GetComponent<CharacterController>();
+        Look_Root = transform.GetChild(0);
     }
     
     // Update is called once per frame
     void Update()
     {
         MoveThePlayer();
+        Sprint();
+        Crouch();
     }
     void MoveThePlayer()
     {
@@ -29,8 +46,9 @@ public class PlayerMovement : MonoBehaviour
         move_direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0, Input.GetAxis(Axis.VERTICAL));
 
         move_direction = transform.TransformDirection(move_direction);//local transform to WorldSpace if any child object altered...
-        //best if we do this....
-        move_direction *= speed * Time.deltaTime;
+                                                                      //best if we do this....
+        
+        move_direction *= move * Time.deltaTime;
         //if (move_direction.x > 0)
         //    print("delta time " + Time.deltaTime); to check the Delta Time
 
@@ -63,7 +81,41 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    void Sprint()
+    {
+        if(Input.GetKeyDown(KeyCode.LeftShift) && !is_Crouching)
+        {
+            move = sprint_speed;
+        }
+        else if(Input.GetKeyUp(KeyCode.LeftShift) && !is_Crouching)
+        {
+            move = run;
+        }
+    }
+    void Crouch()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            if(is_Crouching)    //if crouching is true, then by press C, he needs to stand
+            {
+                Look_Root.localPosition = new Vector3(0, stand_height , 0);
 
+                move = run;
+             
+                is_Crouching = false;
+            }
+            else    //if not crouching by press C, then he needs to Crouch
+            {
+                Look_Root.localPosition = new Vector3(0, crouch_height  , 0);
+                move = crouch_speed;
+
+                is_Crouching = true;
+            }
+        }
+        character_controller.height = Mathf.Lerp(character_controller.height, Look_Root.localPosition.y, 5f * Time.deltaTime);
+        
+
+    }
 
 
 }//class
---
+++ FPS Survival/Assets/Scripts/Player Scripts/PlayerStats.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField]
+    private Image health_stats, stamina_stats;
+    
+    public void Display_Health(float health_val)
+    {
+        health_val /= 100f; 
+        health_stats.fillAmount = health_val;
+    }
+    public void Display_Stamina(float stamina_val)
+    {
+        stamina_val /= 100f; 
+        stamina_stats.fillAmount = stamina_val;
+    }
+
+
+    
+
+
+
+
+
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
