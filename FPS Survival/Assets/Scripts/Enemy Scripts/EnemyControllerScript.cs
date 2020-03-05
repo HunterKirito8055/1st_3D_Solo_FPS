34ffc826106f5351ed940df0cba78e6a36576691
HunterKirito8055 <sirikondavishal8055@gmail.com>
2020-03-05 16:51:09 +0530

    done..

---
+++ FPS Survival/Assets/Scripts/Enemy Scripts/EnemyControllerScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public enum EnemyState
+{
+    PATROL,CHASE,ATTACK
+}
+
+
+public class EnemyControllerScript : MonoBehaviour
+{
+    private EnemyAnimationScript enemy_Anim;    //getting component which is created
+    private NavMeshAgent navAgent;
+
+    private EnemyState enemy_State;
+
+    public float walk_Speed = 0.5f;
+    public float run_Speed = 4f;
+
+    public float chase_Distance = 7f;
+    private float current_Chase_Distance;
+    public float attack_Distance = 1.8f;
+    public float chase_After_Attack_Distance = 2f;
+
+    public float patrol_Radius_min = 20f, patrol_Radius_max = 60f;
+    public float patrol_for_now = 15f;
+    private float patrol_Timer;
+
+    public float wait_before_attack = 2f;
+    private float attack_Timer;
+
+        private Transform target;
+
+    private void Awake()
+    {
+        enemy_Anim = GetComponent<EnemyAnimationScript>();
+        navAgent = GetComponent<NavMeshAgent>();
+
+        target = GameObject.FindWithTag(Tags.PLAYER_TAG).transform;
+    }
+
+
+}//class
+
+
+
+
+
+
+
+
---
+++ FPS Survival/Assets/Scripts/Player Scripts/PlayerFoootSteps.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFoootSteps : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip[] Foot_Clip;
+
+    private AudioSource footstep_sound;
+    private CharacterController _characterController;   // need to get from parent
+    
+    [HideInInspector]
+    public float volMax, volMin;
+    [HideInInspector]
+    public float step_dist;
+
+    private float accumulated_distance;     //travelled distance from previous position to User given position in magnitude
+
+    private void Awake()
+    {
+        _characterController = GetComponentInParent<CharacterController>();
+        footstep_sound = GetComponent<AudioSource>();
+
+    }
+    private void Update()
+    {
+        PlayStepSounds();
+    }
+
+    void PlayStepSounds()
+    {
+        //if player is in the air dont play sound
+        if(!_characterController.isGrounded)
+        {
+            return;
+        }
+
+
+
+        //sqrmagnitude > 0 means moving,, then play sound
+        if(_characterController.velocity.sqrMagnitude>0)
+        {
+            accumulated_distance += Time.deltaTime;//0+time elapsed .... 
+
+            if(accumulated_distance>step_dist)  //we will produce amount of step_dist from playermovement script
+            {
+                footstep_sound.volume = Random.Range(volMin, volMax);
+                footstep_sound.clip = Foot_Clip[Random.Range(0, Foot_Clip.Length)];
+                footstep_sound.Play();
+
+                accumulated_distance = 0f;
+            }
+        }
+        //else not moves, no sounds
+        else
+        {
+            accumulated_distance = 0f;
+        }
+    }
+
+
+}//class
+
+
+
+
+
+
+
+
