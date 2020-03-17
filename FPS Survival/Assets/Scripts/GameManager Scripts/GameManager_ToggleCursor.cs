b0c0b1d10d4c75210aa50044c51ddd311d3d8fa3
seemanshu10 <60509954+seemanshu10@users.noreply.github.com>
2020-03-17 12:33:39 +0530

    menuManagerUpdated

---
+++ FPS Survival/Assets/Scripts/GameManager Scripts/GameManager_ToggleCursor.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace s3
+{
+    public class GameManager_ToggleCursor : MonoBehaviour
+    {
+        private GameManager_Master gameManager_Master;
+        private bool isCursorLocked = true;
+
+        void OnEnable()
+        {
+            SetInitialReference();
+            gameManager_Master.MenuToggleEvent += ToggleCursorState;
+            gameManager_Master.InvertoryUIToggleEvent += ToggleCursorState;
+        }
+
+        void OnDisable()
+        {
+            gameManager_Master.MenuToggleEvent -= ToggleCursorState;
+            gameManager_Master.InvertoryUIToggleEvent -= ToggleCursorState;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckCursorShouldBeLocked();
+        }
+
+        void SetInitialReference()
+        {
+            gameManager_Master = GetComponent<GameManager_Master>();
+        }
+
+        void ToggleCursorState()
+        {
+            isCursorLocked = !isCursorLocked;
+        }
+
+        void CheckCursorShouldBeLocked()
+        {
+            if(isCursorLocked)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
+    }
+}
+
---
+++ FPS Survival/Assets/Scripts/GameManager Scripts/GameManager_TogglePlayer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace s3
+{
+    public class GameManager_TogglePlayer : MonoBehaviour
+    {
+        public MouseControl lookRoot;
+        private GameManager_Master gameManager_Master;
+
+        void OnEnable()
+        {
+            SetInitialReferences();
+            gameManager_Master.MenuToggleEvent += TogglePlayerController;
+            gameManager_Master.InvertoryUIToggleEvent += TogglePlayerController;
+        }
+
+        void OnDisable()
+        {
+            gameManager_Master.MenuToggleEvent -= TogglePlayerController;
+            gameManager_Master.InvertoryUIToggleEvent -= TogglePlayerController;
+        }
+
+        void SetInitialReferences()
+        {
+            gameManager_Master = GetComponent<GameManager_Master>();
+
+        }
+
+        void TogglePlayerController()
+        {
+            if(lookRoot!=null)
+            {
+                lookRoot.enabled = !lookRoot.enabled;
+            }
+        }
+
+    }
+}
+
