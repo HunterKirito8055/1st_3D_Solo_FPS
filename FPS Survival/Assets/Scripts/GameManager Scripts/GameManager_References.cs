e11e297976b8c71dd844a872b54a4c78c3693194
seemanshu10 <60509954+seemanshu10@users.noreply.github.com>
2020-03-17 14:21:31 +0530

    menuManagerInventory

---
+++ FPS Survival/Assets/Scripts/GameManager Scripts/GameManager_References.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace s3
+{
+    public class GameManager_References : MonoBehaviour
+    {
+        public string playerTag;
+        public static string _playerTag;
+
+        public string enemyTag;
+        public static string _enemyTag;
+
+        public static GameObject _player;
+
+        void OnEnable()
+        {
+          if(playerTag=="")
+
+          {
+            Debug.LogWarning("Please type in name of the player tag in the GameManager References ");
+          }
+
+          if (enemyTag == "")
+
+          {
+            Debug.LogWarning("Please type in name of the enemy tag in the GameManager References ");
+          }
+            _playerTag = playerTag;
+            _enemyTag = enemyTag;
+
+            _player = GameObject.FindGameObjectWithTag(_playerTag);
+
+        }
+
+    }
+}
+
---
+++ FPS Survival/Assets/Scripts/GameManager Scripts/GameManager_ToggleInventoryUI.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace s3
+{
+    public class GameManager_ToggleInventoryUI : MonoBehaviour
+    {
+        public bool hasInventory;
+        public GameObject inventoryUI;
+        public string toggleInventoryButton;
+        private GameManager_Master gameManager_Master;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            SetInitialReference();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            CheckForInvertoryUIToggleRequest();
+        }
+
+        void SetInitialReference()
+        {
+            gameManager_Master = GetComponent<GameManager_Master>();
+
+            if (toggleInventoryButton == "")
+            {
+                Debug.LogWarning("Please Type in the name of the button used to toggle the inventory");
+                this.enabled = false;
+
+            }
+        }
+
+        void CheckForInvertoryUIToggleRequest()
+        {
+            if (Input.GetButtonUp(toggleInventoryButton) && !gameManager_Master.isMenuON && !gameManager_Master.isGameOver && hasInventory)
+            {
+                ToggleInventoryUI();
+            }
+        }
+
+        void ToggleInventoryUI()
+        {
+            if (inventoryUI != null)
+            {
+                inventoryUI.SetActive(!inventoryUI.activeSelf);
+                gameManager_Master.isInvertoryUIOn = !gameManager_Master.isInvertoryUIOn;
+                gameManager_Master.CallEventInventoryUIToggle();
+            }
+        }
+    }
+
+}
